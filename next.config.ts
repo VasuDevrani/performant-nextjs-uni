ca00f8912c1a8a37a1a9bd444e847dc77b47870a
VasuDevrani <vasu2002devrani@gmail.com>
2024-11-16 17:23:56 +0530

    added next-bundle-analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,37 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true", // Enable analysis when ANALYZE=true
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: { 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
     unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
     ppr: true,
-    optimizePackageImports: ['lucide-react'],
+    optimizePackageImports: ["lucide-react"],
   },
   webpack: (config) => {
     config.optimization.splitChunks = {
-      chunks: 'all',
+      chunks: "all",
       minSize: 20000,
       maxSize: 70000,
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+          name: "vendors",
+          chunks: "all",
         },
       },
     };
@@ -28,4 +39,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+// export default bundleAnalyzer({ nextConfig });
+export default bundleAnalyzer(nextConfig);
+// export default nextConfig;
